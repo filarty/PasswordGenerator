14601bde35897a32b60650880b0c5c869a9beee7
Artjom <filarty@gmail.com>
2022-10-11 18:21:20 +0200

    add test ceses

--- tests/password_test.py
+++ tests/password_test.py
@@ -2,10 +2,24 @@ from unittest import TestCase
 
 from source.PasswordGenerate import Password
 
+import string
+
 password = Password()
 
 class PasswordTest(TestCase):
     def test_upper_case(self):
         password_string = "".join(password._add_upper_case())
         self.assertTrue(password_string.isupper())
+    
+    def test_lower_case(self):
+        password_string = "".join(password._add_lower_case())
+        self.assertTrue(password_string.islower())
+    
+    def test_numbers_case(self):
+        password_string = "".join(password._add_number_case())
+        self.assertTrue(password_string.isdigit())
         
+    def test_symbols_case(self):
+        password_string = "".join(password._add_symbols_case())
+        self.assertTrue(all((password_string.find(i) for i in string.punctuation)))
+    
